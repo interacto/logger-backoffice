2e13617a76e8d633d4e627db79fd6e57c1158eea
Rémi Daniel <remi.daniel@insa-rennes.fr>
2021-07-15 16:06:48 +0200

    feat(app): created the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import {ApiService} from "./api.service";
+import {LoggingData, UsageLog} from "interacto";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+  public usageLogs: Array<UsageLog> = new Array<UsageLog>();
+  public errorLogs: Array<LoggingData> = new Array<LoggingData>();
   title = 'logger-backoffice';
+
+  constructor(private apiService: ApiService) {}
+
+  ngAfterViewInit(): void {
+    this.refreshData();
+  }
+
+  public refreshData(): void {
+    this.apiService.fetchUsageLogs().then(data => {
+      this.usageLogs = data;
+    });
+    this.apiService.fetchErrorLogs().then(data => {
+      this.errorLogs = data;
+    });
+  }
 }
